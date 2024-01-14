99b4e00968d4beb58a769376b311b4db00515d0d
Osaf Ali <kingsayed02@gmail.com>
2024-01-14 12:23:57 +0100

    Commited on: 2024-01-14 16:53:57

--- README.md
+++ README.md
@@ -37,7 +37,3 @@ Please use GitHacker responsibly and respect the GitHub terms of service. The pu
 ## Contributing
 
 Contributions are welcome! If you have any suggestions, bug reports, or feature requests, please open an issue or submit a pull request.
-
-## License
-
-This project is licensed under the [MIT License](LICENSE).
--- githacker.py
+++ githacker.py
@@ -1,1 +1,27 @@
-print("git baker")
+from datetime import datetime
+from git import Repo, Actor
+import time
+
+PATH = ""
+with open("date.txt", "w") as file:
+    # write the current date and time to the file
+    file.write(str(datetime.now()))
+
+# Initialize a Repo object
+repo = Repo(PATH)
+
+# # Check for changes
+# Check for changes
+if repo.is_dirty():
+    # Add specific file
+    repo.git.add('date.txt')
+    commit_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    # Define author with specific timestamp
+    author = Actor("Osaf Ali ", "kingsayed02@gmail.com")
+    author_time = int(time.mktime(time.strptime(commit_date, '%Y-%m-%d %H:%M:%S')))
+    author_tz = "+0100"  # timezone offset
+
+    # Commit with specific timestamp
+    repo.index.commit(f"Commited on: {commit_date}", author=author, author_date=f"{author_time} {author_tz}")
+else:
+    print("No changes to commit")
